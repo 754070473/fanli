79bed6c625e1ed3483dc62e0630c88bb0690758e
fzqdzh <fzqdyx@163.com>
2016-08-30 08:55:25 +0800

    品牌接口

--- fanli/backend/controllers/IndexController.php
+++ fanli/backend/controllers/IndexController.php
@@ -20,9 +20,9 @@ class IndexController extends CommonController
     public $enableCsrfValidation = false;
     public function actionIndex()
     {
-        $url = $this -> apiUrl('Index','index');
-        $data = array('name' => '张三','id' => 1);
-        print_r($this -> CurlPost($url , $data));
+        // $url = $this -> apiUrl('Index','index');
+        // $data = array('name' => '张三','id' => 1);
+        // print_r($this -> CurlPost($url , $data));
         return $this->render('index.html');
     }
 }
