996eb61d056ff2d203dea661274da5258fa00046
dongmengtao <1653162261@qq.com>
2016-08-31 08:02:14 +0800

    管理员列表

---
+++ api/Application/Admin/Status/Success.class.php.BASE.php
@@ -0,0 +1,15 @@
+<?php
+namespace Admin\Status;
+/**
+ * 成功时候的提示
+ */
+class Success {
+
+    //登录成功
+    const LOGIN_SUCCESS = 0;
+    const LOGIN_SUCCESS_MSG = '登录成功!';
+    
+    //查询成功
+    const SELECT_SUCCESS = 0;
+    const SELECT_SUCCESS_MSG = '查询成功!';
+}
---
+++ api/Application/Admin/Status/Success.class.php.LOCAL.php
@@ -0,0 +1,21 @@
+<?php
+namespace Admin\Status;
+/**
+ * 成功时候的提示
+ */
+class Success {
+
+    //登录成功
+    const LOGIN_SUCCESS = 0;
+    const LOGIN_SUCCESS_MSG = '登录成功!';
+    
+    //查询成功
+    const SELECT_SUCCESS = 0;
+    const SELECT_SUCCESS_MSG = '查询成功!';
+
+
+
+    //管理员添加
+    const ADMIN_ADD = 0;
+    const ADMIN_ADD_MSG = '添加成功';
+}
