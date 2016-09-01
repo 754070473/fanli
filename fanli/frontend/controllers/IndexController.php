463baaeea00530c604586a0fd9d0044f0f9c6b46
fzqdzh <fzqdyx@163.com>
2016-09-01 10:04:11 +0800

    根据分类id查询该分类下所有正在进行促销活动的品牌

--- fanli/frontend/controllers/IndexController.php
+++ fanli/frontend/controllers/IndexController.php
@@ -33,4 +33,14 @@ class IndexController extends CommonController
         $arr = $this -> databasesSelect($table , 0 ,'pid = 0');
         return $arr;
     }
+
+    //根据分类id查询该分类下所有正在进行促销活动的品牌
+    public function actionClass_activity($cla_id = 1)
+    {
+    	$table = array(['table1'=>'fanli_goods','table2'=>'fanli_classify','join'=>'cla_id'],['table1'=>'fanli_goods','table2'=>'fanli_activity','join'=>'act_id']);
+    	$arr = $this -> databasesSelect($table , 0 ,'fanli_classify.cla_id = '.$cla_id);
+    	 // print_r($arr);die;
+    	return $arr;
+    }
+
 }
